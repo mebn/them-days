8ada4decd205ab18d1cd9615d0f4c54e49f2b57b
Marcus Nilszén <nilszen.marcus@gmail.com>
2025-10-14 19:32:00 +0200

    widget

---
+++ themDaysWidget/themDaysWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  themDaysWidgetBundle.swift
+//  themDaysWidget
+//
+//  Created by Marcus Nilszén on 2025-10-14.
+//
+
+import SwiftUI
+import WidgetKit
+
+@main
+struct themDaysWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        themDaysWidget()
+    }
+}
